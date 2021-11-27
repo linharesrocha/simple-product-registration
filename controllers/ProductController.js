4867f26967646396c46df29cb646649d4531424a
Guilherme Linhares Rocha <guilhermelinharesr@gmail.com>
2021-11-27 13:57:57 -0300

    atualizando para o mongoose

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -3,7 +3,7 @@ const Product = require('../models/Product')
 module.exports = class ProductController {
     static async showProducts(req, res) {
 
-        const products = await Product.getProducts()
+        const products = await Product.find().lean()
 
         res.render('products/all', {products})
     }
@@ -12,16 +12,16 @@ module.exports = class ProductController {
         res.render('products/create')
     }
 
-    static createProductPost(req, res) {
+    static async createProductPost(req, res) {
 
         const name = req.body.name
         const image = req.body.image
         const price = req.body.price
         const description = req.body.description
 
-        const product = new Product(name, image, price, description)
+        const product = new Product({name, image, price, description})
 
-        product.save()
+        await product.save()
 
         res.redirect('/products')
     }
@@ -30,7 +30,9 @@ module.exports = class ProductController {
 
         const id = req.params.id
 
-        const product = await Product.getProductById(id)
+        const product = await Product.findById(id).lean()
+
+        console.log(product)
 
         res.render('products/product', {product})
     }
@@ -39,7 +41,7 @@ module.exports = class ProductController {
 
         const id = req.params.id
 
-        await Product.removeProductById(id)
+        await Product.deleteOne({_id: id})
 
         res.redirect('/products')
     }
@@ -48,7 +50,7 @@ module.exports = class ProductController {
 
         const id = req.params.id
 
-        const product = await Product.getProductById(id)
+        const product = await Product.findById(id).lean()
 
         res.render('products/edit', { product })
     }
@@ -61,9 +63,9 @@ module.exports = class ProductController {
         const price = req.body.price
         const description = req.body.description
 
-        const product = new Product(name, image, price, description)
+        const product = {name, image, price, description}
 
-        await product.updateProduct(id)
+        await Product.updateOne({ id: id}, product)
 
         res.redirect('/products')
     }
--- db/conn.js
+++ db/conn.js
@@ -1,19 +1,10 @@
-const { MongoClient } = require('mongodb')
+const mongoose = require('mongoose')
 
-const uri = "mongodb://localhost:27017/testemongodb"
-
-const client = new MongoClient(uri)
-
-async function run() {
-    try{
-
-        await client.connect()
-        console.log('Conectado ao MongoDB!')
-    }catch(err) {
-        console.log(err)
-    }
+async function main() {
+    await mongoose.connect('mongodb://localhost:27017/testemongoose2')
+    console.log('Conectado ao MongoDB com o Mongoose!')
 }
 
-run()
+main().catch((err) => console.log(err))
 
-module.exports = client
+module.exports = mongoose
